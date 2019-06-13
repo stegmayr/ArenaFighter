2aa102cf2ef289edc28d10e836d80c34e2e3a2db
Alex Stegmayr <a.stegmayr@outlook.com>
2019-06-13 13:45:55 +0200

    Add project files.

---
+++ ArenaFighter/Fighter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaFighter
+{
+    class Fighter
+    {
+        public string name;
+        public int strength;
+        public int damage;
+        public int health;
+
+        public Fighter(string name)
+        {
+            Random randomInt = new Random();
+            this.name = name;
+            this.strength = randomInt.Next(1, 11);
+            this.damage = randomInt.Next(1, 6);
+            this.health = randomInt.Next(1, 16);
+        }
+    }
+}
---
+++ ArenaFighter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArenaFighter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    } // End of class
+} // End of namespace
